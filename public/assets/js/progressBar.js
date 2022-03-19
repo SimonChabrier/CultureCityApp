1aef85def18669ced9e30c1e17934ac56fbbf314
SimonChabrier <simonchabrier@gmail.com>
2022-03-20 00:32:54 +0100

    add final class and message

--- public/assets/js/progressBar.js
+++ public/assets/js/progressBar.js
@@ -1,23 +1,43 @@
+//first think to call progressBar.js and Jquery in header
 
-var desiredLength = 10; 
+//set max text lenght in caracters
+const desiredLength = 5; 
+//var message = $("#prog").val();
 
-// Listen to the change, keyup & paste events.
+// Listen changes on keyup & paste events.
 $('#text-box').on('change keyup paste', function() {
-  // Figure out the length of the input value
-  var textLength = $('#text-box').val().length;
+  // We get the length of the input value
+  let textLength = $('#text-box').val().length;
 
   // Calculate the percentage
-  var percent = (textLength / desiredLength) * 100;
-  // Limit the percentage to 100.
-  if (percent > 100) {
-    percent = 100;
-  }
+  let percent = (textLength / desiredLength) * 100;
+  
+    if (percent > 100) {
+        // Limit the percentage to 100.
+        percent = 100;
+
+        //set progress bar end css class
+        let message = "100%";
+        $('#prog').append(message);
+        $('.progress-bar').addClass('final-progress-bar');
+
+    }
+
+    if (percent < 100) {
+
+       //todo message.remove()
+        $('.progress-bar').addClass('progress-bar').removeClass('final-progress-bar');
+    
+    }
+
 
   // Set the width of the bar based on the percentage.
-  //$('.progress-bar').width(percent + '%');
+  $('.progress-bar').width(percent + '%');
 
   // Animate the width of the bar based on the percentage.
-  $('.progress-bar').animate({
+  /* $('.progress-bar').animate({
     width: percent + '%'
-  }, 0)
+  }, 0) */
 });
+
+
