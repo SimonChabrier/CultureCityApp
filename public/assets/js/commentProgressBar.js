1d5cd8a7ad8cccfcabfe3823aab88416ded2de19
SimonChabrier <simonchabrier@gmail.com>
2022-03-20 16:06:42 +0100

    fix comments for progressbar

--- public/assets/js/commentProgressBar.js
+++ public/assets/js/commentProgressBar.js
@@ -1,17 +1,24 @@
-//first think to call progressBar.js and Jquery in header
+//* first think to call progressBar.js and Jquery in header
 
-//set max text lenght in caracters
+// set max text lenght in caracters for input form fields
+// this is in link with an assert in Post.php on property $content
 const postLength = 280; 
 
-// Listen changes on keyup & paste events.
+/**
+ * Method:
+ * Listen changes on keyup & paste events.
+ * Change progress bar state in %
+ * Change background css properties
+ */
 $('#text-box').on('change keyup paste', function() {
 
-  // We get the length of the input value
+  // We get the length of the form field input value by form field Id
   let textLength = $('#text-box').val().length;
 
-  // Calculate the percentage
+  // We calculate the percentage value
   let percent = (textLength / postLength) * 100;
 
+     // set the different css property for progressbar background
     if (percent > 30) {
         $('.progress-bar').css("background", "#ffc107");
     }
@@ -33,6 +40,7 @@ $('#text-box').on('change keyup paste', function() {
         
         // Limit the percentage to 100.
         percent = 100;
+        // Display the limit caracters lenght message
         $('.message').css("visibility", "visible");
     }
 
@@ -40,7 +48,7 @@ $('#text-box').on('change keyup paste', function() {
     // Set the width of the bar based on the percentage.
     $('.progress-bar').width(percent + '%');
 
-        // UncommentAnimate to the width of the bar based on the percentage.
+        // Uncomment for animate to the width of the bar based on the percentage.
         /*
         $('.progress-bar').animate({
             width: percent + '%'
